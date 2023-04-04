4f104b70730f8d37751dc1e992aa7f92a6b72744
Rifat Hossain <smrifat1411@gmail.com>
2023-04-05 02:00:15 +0600

    emitter example with http server

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-// Event-Driven Programming in Nodejs 
-// same as onclick, onchange, etc js event 
+// Event-Driven Programming in Nodejs
+// same as onclick, onchange, etc js event
 
+// const EventEmitter = require("events")
 
-const EventEmitter = require("events")
+// const myEvent = new EventEmitter;
 
-const myEvent = new EventEmitter;
+// myEvent.on("eventName",(name,age)=>{
+//     console.log(`I got response with name : ${name} and age :${age}`);
+// })
 
+// myEvent.emit("eventName", "Rifat",23)
 
-myEvent.on("eventName",(name,age)=>{
-    console.log(`I got response with name : ${name} and age :${age}`);
-})
+const http = require("http");
 
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
 
+// Using Event Emitter API
+const server = http.createServer();
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on("request", (req, res) => {
+  res.end("Welcome");
+});
 
-
-
-myEvent.emit("eventName", "Rifat",23)
+server.listen(5000);
