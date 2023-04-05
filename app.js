0249c2af14eb45c669d3b770adedce58b9cb0ed3
Rifat Hossain <smrifat1411@gmail.com>
2023-04-05 21:00:06 +0600

    http message overview with basic code example2

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "content-type": "text/html",
-  });
-  res.write("<h1>Home Page</h1>");
-
-  // we can also directly write html or body in res.end() instead of res.write
-  res.end();
+  const url = req.url;
+  // Home Page
+  if (url === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+  } else if (url === "/contact") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.write("<h1>Contact Page</h1>");
+    res.end();
+  }
+  // About Page
+  else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.write("<h1>Request Page Not Found</h1>");
+    res.end();
+  }
 });
 
 server.listen(2000);
