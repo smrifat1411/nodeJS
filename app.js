5c3e32462604b419b70ccb2ffda15244d98da626
Rifat Hossain <smrifat1411@gmail.com>
2023-04-05 19:43:29 +0600

    http message overview with basic code example

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-// Event-Driven Programming in Nodejs
-// same as onclick, onchange, etc js event
-
-// const EventEmitter = require("events")
-
-// const myEvent = new EventEmitter;
-
-// myEvent.on("eventName",(name,age)=>{
-//     console.log(`I got response with name : ${name} and age :${age}`);
-// })
-
-// myEvent.emit("eventName", "Rifat",23)
-
 const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   res.end('Welcome')
-// })
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "content-type": "text/html",
+  });
+  res.write("<h1>Home Page</h1>");
 
-// Using Event Emitter API
-const server = http.createServer();
-// emits request event
-// subcribe to it / listen for it / respond to it
-server.on("request", (req, res) => {
-  res.end("Welcome");
+  // we can also directly write html or body in res.end() instead of res.write
+  res.end();
 });
 
-server.listen(5000);
+server.listen(2000);
---
+++ event-emitter.js
@@ -0,0 +1,30 @@
+// Event-Driven Programming in Nodejs
+// same as onclick, onchange, etc js event
+
+// const EventEmitter = require("events")
+
+// const myEvent = new EventEmitter;
+
+// myEvent.on("eventName",(name,age)=>{
+//     console.log(`I got response with name : ${name} and age :${age}`);
+// })
+
+// myEvent.emit("eventName", "Rifat",23)
+
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer().on("request", (req, res) => {
+  res.end("Welcome");
+});
+// emits request event
+// subcribe to it / listen for it / respond to it
+// server.on("request", (req, res) => {
+//   res.end("Welcome");
+// });
+
+server.listen(2000);
